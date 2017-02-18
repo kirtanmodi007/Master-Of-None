816f0f1fa4e6ba79c689f1bbe1b21b1bc0a2ca44
akshatdoshi <akshat9731@gmail.com>
2017-02-18 20:08:06 +0530

    Update songs.m

--- song picker/songs.m
+++ song picker/songs.m
@@ -1,17 +1,36 @@
-prompt = 'Enter your emotion? ';
-x = input(prompt,'s')
+%prompt = 'Enter your emotion? ';
+%x = input(prompt,'s')
+function songs(filename)
+x = filename;
 s1 = 'happy';
 s2 = 'sad';
+s3 = 'natural';
 
-if  strcmp(x,s2)
-    song1 = randi(10,1);
-    file_name=dir('E:\hackathon\Speech Recognition in MATLAB using correlation\Speech Recognition in MATLAB using correlation\sad\sad*.mp3');
-    x=randi(5,1) %suppose there are 7 image
-    disp(x);
-    im=strcat('E:\hackathon\Speech Recognition in MATLAB using correlation\Speech Recognition in MATLAB using correlation\sad\',file_name(x).name);
+if strcmp(x,s1)
+    
+    file_name=dir('E:\hackathon\Speech Recognition in MATLAB using correlation\Speech Recognition in MATLAB using correlation\happy\happy*.mp3');
+    y=randi(2,1);
+    %disp(x);
+    im=strcat('E:\hackathon\Speech Recognition in MATLAB using correlation\Speech Recognition in MATLAB using correlation\happy\',file_name(y).name);
     [a,Fs] = audioread(im);  
     sound(a,Fs);
-elseif strcmp(x,s1)
-    [b,Fs] = audioread('happy.mp3');
-    sound(b,Fs);
+  
+elseif  strcmp(x,s2)
+  
+    file_name=dir('E:\hackathon\Speech Recognition in MATLAB using correlation\Speech Recognition in MATLAB using correlation\sad\sad*.mp3');
+    y=randi(5,1); %suppose there are 7 image
+    %disp(x);
+    im=strcat('E:\hackathon\Speech Recognition in MATLAB using correlation\Speech Recognition in MATLAB using correlation\sad\',file_name(y).name);
+    [b,Fs] = audioread(im);  
+    sound(b,Fs); 
+    
+elseif  strcmp(x,s3)
+  
+    file_name=dir('E:\hackathon\Speech Recognition in MATLAB using correlation\Speech Recognition in MATLAB using correlation\natural\natural*.mp3');
+    y=randi(2,1); %suppose there are 7 image
+    %disp(x);
+    im=strcat('E:\hackathon\Speech Recognition in MATLAB using correlation\Speech Recognition in MATLAB using correlation\natural\',file_name(y).name);
+    [c,Fs] = audioread(im);  
+    sound(c,Fs); 
+    
 end
