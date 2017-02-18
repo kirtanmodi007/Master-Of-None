4632561ca6b0aab6e9d05aa94c9f08674811f3c9
akshatdoshi <akshat9731@gmail.com>
2017-02-18 10:01:24 +0530

    Update main.m

--- main.m
+++ main.m
@@ -1,15 +1,11 @@
-% This is a sample script
-% It demos the face expression recognition using the Eigenface method
+
 
 disp('Face Expression Recognition Demo Using the Eigenface Method')
-%disp('After your face appeared in the camera window, please left click your mouse to take a picture of youself')
-%disp('Please save your cute picture to D:\CS6527-Computer Vision\Final Project\Project Code\TestImage')
 
-%pictureCapture
 
-TrainImagePath = 'C:\Users\Manoj\Desktop\Facial-Expression-Recognition-master\Facial-Expression-Recognition-master\Project Code\TrainingImage';
-TestImagePath = 'C:\Users\Manoj\Desktop\Facial-Expression-Recognition-master\Facial-Expression-Recognition-master\Project Code\TestImage';
-LabelPath = 'C:\Users\Manoj\Desktop\Facial-Expression-Recognition-master\Facial-Expression-Recognition-master\Project Code\ImageLabel.txt';
+TrainImagePath = 'C:\Users\Manoj\Desktop\master_of_none\newemoji\Facial-Expression-Recognition-master\Project Code\TrainingImage';
+TestImagePath = 'C:\Users\Manoj\Desktop\master_of_none\newemoji\Facial-Expression-Recognition-master\Project Code\TestImage';
+LabelPath = 'C:\Users\Manoj\Desktop\master_of_none\newemoji\Facial-Expression-Recognition-master\Project Code\ImageLabel.txt';
 
 [NumTrainImg,TrainImg] = loadImage( TrainImagePath );
 [NumTestImg,TestImg] = loadImage( TestImagePath );
@@ -25,13 +21,15 @@ RecognizedExpression = strcat(int2str(minDistIndex),'.jpg');
     
     % export the matched label
     Best_Match = cell2mat(imageLabel{1,1}(minDistIndex));
-    ExprLabel = cell2mat(imageLabel{1,2}(minDistIndex));
+ 
 
-str1 = strcat('Your face expression is like this one:  ',RecognizedExpression);
-str2 = strcat('It tells me that you are:  ',ExprLabel);
-disp(str1)
-disp(str2)
 
-%SelectedImage = strcat(TrainImagePath,'\',RecognizedExpression);
-%SelectedImage = imread(SelectedImage);
-%imshow(SelectedImage)
+for i=1:length(minDistIndex)
+    str1 = strcat('Your face expression is like this one:  ',RecognizedExpression(i));
+       ExprLabel = cell2mat(imageLabel{1,2}(minDistIndex(i)));
+
+    str2 = strcat('It tells me that you are:  ',ExprLabel);
+    disp(str1)
+
+   disp(str2)
+end
